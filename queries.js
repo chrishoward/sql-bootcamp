7eb368deb5c74f5610c15c76c0e4e6cb8a8d992f
Christopher Howard <chrisau233@gmail.com>
2020-01-06 20:06:42 +1000

    add practice and challenge queries

--- queries.js
+++ queries.js
@@ -3,3 +3,43 @@ SELECT first_name, last_name, email
 FROM customer;
 
 // challenge 2
+SELECT DISTINCT rating
+FROM film;
+
+// challenge 3
+SELECT email
+FROM customer
+WHERE first_name = 'Nancy'
+AND last_name = 'Thomas'
+
+// challenge 4
+SELECT description
+FROM film
+WHERE title = 'Outlaw Hanky';
+
+// challenge 5
+SELECT phone
+FROM address
+WHERE address = '259 Ipoh Drive' OR address2 = '259 Ipoh Drive';
+
+// practice
+SELECT COUNT(DISTINCT length)
+FROM film;
+
+// practice
+SELECT *
+FROM film
+LIMIT 5;
+
+// challenge 6
+SELECT customer_id, amount
+FROM payment
+ORDER BY amount DESC
+LIMIT 10;
+
+// challenge 7
+SELECT film_id, title
+FROM film
+ORDER BY film_id ASC
+LIMIT 5;
+
