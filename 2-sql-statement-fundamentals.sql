5d7a71b4c0d5c409edab66315fe4e7eca61e31f3
Chris Howard <chrisau233@gmail.com>
2020-09-22 20:19:14 +1000

    Change to .sql file

--- 2-sql-statement-fundamentals.sql
+++ 2-sql-statement-fundamentals.sql
@@ -1,68 +1,68 @@
-// challenge 1
+-- challenge 1
 SELECT first_name, last_name, email
 FROM customer;
 
-// challenge 2
+-- challenge 2
 SELECT DISTINCT rating
 FROM film;
 
-// challenge 3
+-- challenge 3
 SELECT email
 FROM customer
 WHERE first_name = 'Nancy'
 AND last_name = 'Thomas'
 
-// challenge 4
+-- challenge 4
 SELECT description
 FROM film
 WHERE title = 'Outlaw Hanky';
 
-// challenge 5
+-- challenge 5
 SELECT phone
 FROM address
 WHERE address = '259 Ipoh Drive' OR address2 = '259 Ipoh Drive';
 
-// practice
+-- practice
 SELECT COUNT(DISTINCT length)
 FROM film;
 
-// practice
+-- practice
 SELECT *
 FROM film
 LIMIT 5;
 
-// challenge 6
+-- challenge 6
 SELECT customer_id, amount
 FROM payment
 ORDER BY amount DESC
 LIMIT 10;
 
-// challenge 7
+-- challenge 7
 SELECT film_id, title
 FROM film
 ORDER BY film_id ASC
 LIMIT 5;
 
-// practice
+-- practice
 SELECT inventory_id, film_id
 FROM inventory
 WHERE inventory_id
 BETWEEN 3 AND 9;
 
-// challenge 8
+-- challenge 8
 SELECT COUNT(*)
 FROM payment
 WHERE amount > 5.00;
 
-// challenge 9
+-- challenge 9
 SELECT COUNT(*)
 FROM actor
 WHERE first_name LIKE 'P%';
              
-// challenge 10
+-- challenge 10
 SELECT COUNT(DISTINCT(district))
 FROM address;
 
-// challenge 11
+-- challenge 11
 
 
