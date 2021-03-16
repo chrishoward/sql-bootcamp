0c1a02e389197f02120391ea3e9bfb683111784e
Chris Howard <chrisau233@gmail.com>
2021-03-16 17:57:27 +1000

    Further course progress

--- 2-sql-statement-fundamentals.sql
+++ 2-sql-statement-fundamentals.sql
@@ -335,3 +335,61 @@ INNER JOIN cd.members AS m
 ON b.memid = m.memid
 WHERE m.firstname = 'David'
 AND m.surname = 'Farrell';
+
+-- practice
+CREATE TABLE account(
+	user_id SERIAL PRIMARY KEY,
+	username VARCHAR(50) UNIQUE NOT NULL,
+	password VARCHAR(50) NOT NULL,
+	email VARCHAR(250) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+	last_login TIMESTAMP 
+)
+
+CREATE TABLE job(
+	job_id SERIAL PRIMARY KEY,
+	job_name VARCHAR(200) UNIQUE NOT NULL
+)
+
+CREATE TABLE account_job(
+	user_id INTEGER REFERENCES account(user_id),
+	job_id INTEGER REFERENCES job(job_id),
+	hire_date TIMESTAMP
+)
+
+INSERT INTO account(username, password, email, created_on)
+VALUES
+	('Jose', 'password', 'jose@mail.com', CURRENT_TIMESTAMP);
+
+INSERT INTO job(job_name)
+VALUES
+	('Astronaut'),
+	('President');
+
+INSERT INTO account_job(user_id, job_id, hire_date)
+VALUES
+	(1, 1, CURRENT_TIMESTAMP);
+
+UPDATE account
+SET last_login = CURRENT_TIMESTAMP;
+
+UPDATE account
+SET last_login = created_on;
+
+UPDATE account_job
+SET hire_date = account.created_on
+FROM account
+WHERE account_job.user_id = account.user_id;
+
+UPDATE account
+SET last_login = CURRENT_TIMESTAMP
+RETURNING email, created_on, last_login;
+
+INSERT INTO job(job_name)
+VALUES
+	('Cowboy');
+
+DELETE FROM job
+WHERE job_name = 'Cowboy'
+RETURNING job_id, job_name;
+
